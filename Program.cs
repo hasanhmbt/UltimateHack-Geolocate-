db3e3f5c092e44a6fa84f48570fe06f903e1abf6
hasan <86876834+hasanhmbt@users.noreply.github.com>
2024-05-02 20:55:02 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Drawing;
+using System.Net.Http;
+using System.Security.Principal;
+using Newtonsoft.Json;
+using UltimateHack_Geolocate_;
+using static System.Net.WebRequestMethods;
+
+
+
+namespace superhack
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Console.Title = "darkwebhack";
+            Console.Write("Enter IP Adress:");
+            string ip = Console.ReadLine();
+
+            string url = $"https://ipinfo.io/{ip}/json";
+
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    Console.WriteLine("[+] the bluetooth device is connected successfully🤤💀😎😍🥰🤩😪🤤😈👽👾🐳");
+
+                    string respondData = await response.Content.ReadAsStringAsync();
+
+                    VeryImportantData ipInfo = JsonConvert.DeserializeObject<VeryImportantData>(respondData);
+
+
+                    // Console.Clear();
+
+                    Console.WriteLine($"Country: {ipInfo.country}");
+                    Console.WriteLine($"City: {ipInfo.city}");
+                    Console.WriteLine($"Region: {ipInfo.region}");
+                    Console.WriteLine($"Loc: {ipInfo.loc}");
+                    Console.WriteLine($"Org: {ipInfo.org}");
+                    Console.WriteLine($"Postal: {ipInfo.postal}");
+                    Console.WriteLine($"Timezone: {ipInfo.timezone}");
+                    string[] Coords = ipInfo.loc.Split(',');
+                    Console.WriteLine($"Gooogle Maps: https://www.google.com/maps/?q={Coords[0]},{Coords[1]}");
+                }
+                catch (HttpRequestException ex)
+                {
+
+                    Console.WriteLine($"Error:{ex.Message}");
+                }
+            }
+
+
+        }
+
+    }
+
+}
---
+++ VeryImportantData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimateHack_Geolocate_
+{
+    public class VeryImportantData
+    {
+        public string country { get; set; }
+        public string city { get; set; }
+        public string region { get; set; }
+        public string loc { get; set; }
+        public string org { get; set; }
+        public string postal { get; set; }
+        public string timezone { get; set; }
+
+    }
+}
